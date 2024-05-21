3cbc2e760f8f9860b4b62fe7f81d80c36454085a
Vladyslav <kolesnikvladislav552@gmail.com>
2024-05-21 20:17:05 +0300

    Creating Tabs.jsx component to group Tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import Header from './components/Header.jsx';
 import './styles/App.scss';
+import Tabs from './components/Tabs.jsx';
 
 function App() {
   const [viewMode, setViewMode] = useState('split');
-
   return (
     <div className="container">
       <Header
         viewMode={viewMode}
         setViewMode={setViewMode}
       />
+      <Tabs setViewMode={setViewMode}/>
+
       <div className="main">
 
       </div>
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,20 @@
+import { PropTypes } from 'prop-types';
+import Tab from "./Tab.jsx";
+
+export default function Tabs({ setViewMode }) {
+  return (
+    <section className="tabs">
+      <Tab setViewMode={setViewMode} mode={'editor'}>
+        Editor
+      </Tab>
+
+      <Tab setViewMode={setViewMode} mode={'preview'}>
+        Preview
+      </Tab>
+    </section>
+  );
+}
+
+Tabs.PropTypes = {
+  setViewMode: PropTypes.func,
+};
