6e02fe12d4538eb0e32384399fc72d12588b5833
Vladyslav <kolesnikvladislav552@gmail.com>
2024-05-21 20:40:07 +0300

    Styling Tab.jsx and Tabs.jsx

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,20 +1,26 @@
 import { PropTypes } from 'prop-types';
-import Tab from "./Tab.jsx";
+import Tab from './Tab.jsx';
 
 export default function Tabs({ setViewMode }) {
   return (
     <section className="tabs">
-      <Tab setViewMode={setViewMode} mode={'editor'}>
+      <Tab
+        setViewMode={setViewMode}
+        mode={'editor'}
+      >
         Editor
       </Tab>
 
-      <Tab setViewMode={setViewMode} mode={'preview'}>
+      <Tab
+        setViewMode={setViewMode}
+        mode={'preview'}
+      >
         Preview
       </Tab>
     </section>
   );
 }
 
-Tabs.PropTypes = {
+Tabs.propTypes = {
   setViewMode: PropTypes.func,
 };
