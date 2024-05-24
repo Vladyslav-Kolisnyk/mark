df9b092c665475a1cf49f6344376d70eebfaf234
Vladyslav <kolesnikvladislav552@gmail.com>
2024-05-24 20:56:21 +0300

    Creating ClipboardButton.jsx which allows to copy entered text to the clipboard

---
+++ src/components/ClipboardButton.jsx
@@ -0,0 +1,47 @@
+import { useContext, useState } from 'react';
+import { MarkdownInputContext } from './MarkdownInputContext.jsx';
+import HeaderButton from './HeaderButton.jsx';
+
+export default function ClipboardButton() {
+  const [markdownInput] = useContext(MarkdownInputContext);
+  const [copied, setCopied] = useState(false);
+
+  function copyToClipboard(text) {
+    navigator.clipboard.writeText(text);
+
+    setCopied(true);
+    setTimeout(setCopied, 1000, false);
+  }
+
+  return (
+    <HeaderButton
+      handleClick={copyToClipboard}
+      args={markdownInput}
+    >
+      {copied
+        ? (<svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e8eaed"
+          >
+            <path
+              d="m424-318 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h168q13-36 43.5-58t68.5-22q38 0 68.5 22t43.5 58h168q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm280-670q13 0 21.5-8.5T510-820q0-13-8.5-21.5T480-850q-13 0-21.5 8.5T450-820q0 13 8.5 21.5T480-790Z"
+            />
+          </svg>)
+        : (<svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e8eaed"
+          >
+            <path
+              d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h168q13-36 43.5-58t68.5-22q38 0 68.5 22t43.5 58h168q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm80-160h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Zm200-190q13 0 21.5-8.5T510-820q0-13-8.5-21.5T480-850q-13 0-21.5 8.5T450-820q0 13 8.5 21.5T480-790Z"
+            />
+          </svg>)
+    }
+    </HeaderButton>
+  );
+}
--- src/components/EditButtons.jsx
+++ src/components/EditButtons.jsx
@@ -1,7 +1,9 @@
+import ClipboardButton from './ClipboardButton.jsx';
+
 export default function EditButtons() {
   return (
     <ul className="edit-buttons">
-
+      <ClipboardButton/>
     </ul>
   );
 }
