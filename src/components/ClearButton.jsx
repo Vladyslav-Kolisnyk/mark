aab81a1cb35f50546a2dcca4f6908c260b341671
Vladyslav <kolesnikvladislav552@gmail.com>
2024-05-27 10:48:12 +0300

    Fix icons are not displayed in safari

--- src/components/ClearButton.jsx
+++ src/components/ClearButton.jsx
@@ -16,6 +16,8 @@ export default function ClearButton() {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
+          height="24px"
+          width="24px"
         >
           <path
             d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z"
