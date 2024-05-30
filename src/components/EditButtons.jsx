8a98ba54f126f7de84fe8c5fb595282cf4ca72b7
Vladyslav <kolesnikvladislav552@gmail.com>
2024-05-30 13:32:40 +0300

    Creating CheatButton.jsx that allows to display cheat sheet

--- src/components/EditButtons.jsx
+++ src/components/EditButtons.jsx
@@ -1,9 +1,11 @@
+import CheatButton from './CheatButton.jsx';
 import ClearButton from './ClearButton.jsx';
 import ClipboardButton from './ClipboardButton.jsx';
 
 export default function EditButtons() {
   return (
     <ul className="edit-buttons">
+      <li><CheatButton/></li>
       <li><ClipboardButton/></li>
       <li><ClearButton/></li>
     </ul>
